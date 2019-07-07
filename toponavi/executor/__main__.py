f79b3ffeb3f58824fbc03cfc9bef34ffa0f76695
胡玮文 <huww98@outlook.com>
2019-07-07 12:03:57 +0800

    logging to journald

--- toponavi/executor/__main__.py
+++ toponavi/executor/__main__.py
@@ -1,9 +1,12 @@
 import pathlib
 import asyncio
 import logging
+import signal
+import logging
 
 import yaml
 import wiringpi
+from systemd import journal
 
 from .zmqadapter import ZmqAdapter
 
@@ -13,10 +16,18 @@ async def main():
     with (pathlib.Path(__file__).parent/'config.yml').open('r', newline='') as ymlfile:
         config = yaml.safe_load(ymlfile)
 
-    zmq_adapter = ZmqAdapter(config)
-    await zmq_adapter.run()
+    with ZmqAdapter(config) as zmq_adapter:
+        run_task = asyncio.ensure_future(zmq_adapter.run())
+        signal.signal(signal.SIGTERM, lambda *args: run_task.cancel())
+        await run_task
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
+    logging.root.addHandler(journal.JournalHandler())
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    try:
+        loop.run_until_complete(main())
+    except asyncio.CancelledError:
+        pass
+    logging.debug("main ended")
