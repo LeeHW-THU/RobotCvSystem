7ef1126b749e03c442f90bb2d0dd9c76d61c6e4b
LYT <565735742@qq.com>
2019-07-08 12:47:36 +0800

    修改了运行时可能有的bug，尚未解决与cc的通信问题

--- toponavi/Location/PF_Location.py
+++ toponavi/Location/PF_Location.py
@@ -8,7 +8,9 @@ import scipy.stats
 from numpy.random import uniform, randn, random
 
 class PF_Location():
+
     def __init__(self):
+
         self.mark_endpoint = 'ipc:///run/toponavi/MarkerDetector/Location.ipc'
         self.cc_endpoint = 'ipc:///run/toponavi/CentralControl/Location.ipc'
         self.loc_endpoint = 'ipc:///run/toponavi/Location/CentralControl.ipc'
@@ -19,6 +21,7 @@ class PF_Location():
         
         socket_dir = pathlib.Path("/run/toponavi/Location")
         socket_dir.mkdir(parents=True, exist_ok=True)
+
         self.marker_data = {}
         self.iddata = {}
         self.time1 = 0.
@@ -31,15 +34,16 @@ class PF_Location():
         self.location = ''
         self.mu = 0
 
+    # 创建粒子
     def create_uniform_particles(self, x_range, N):
         particles = np.random.rand(N)*x_range
         return particles
-
+    # 状态转化器
     def predict(self, particles, u, std, dt, dir):
         N = len(particles)
         particles += u*dt*dir + (randn(N) * std) 
 
-
+    #更新粒子权值
     def update(self,particles, weights, z, R, landmarks):
         weights.fill(1.)
         for i, landmark in enumerate(landmarks):
@@ -48,19 +52,19 @@ class PF_Location():
 
         weights += 1.e-300  # avoid round-off to zero
         weights /= sum(weights)  # normalize
-
-
+  
+ #  加权平均求预计位置
     def estimate(self,particles, weights):
         pos = particles
         mean = np.average(pos, weights=weights, axis=0)
         var = np.average((pos - mean) ** 2, weights=weights, axis=0)
         return mean, var
 
-
+#   判断是否进行重采样的条件
     def neff(self,weights):
         return 1. / np.sum(np.square(weights))
 
-
+# 重采样
     def simple_resample(self,particles, weights):
         N = len(particles)
         cumulative_sum = np.cumsum(weights)
@@ -74,7 +78,7 @@ class PF_Location():
 
 
 
-
+#   确定小车运动方向
     def set_direction(self):
         if len(self.marker_data["euAngles"]):
             if(self.marker_data["euAngles"][0][0]<=175 and self.marker_data["euAngles"][0][0]>=95):
@@ -82,22 +86,22 @@ class PF_Location():
             if(self.marker_data["euAngles"][0][0]<=-95 and self.marker_data["euAngles"][0][0]>=-175):
                 self.direction = -1
 
-
+#   评估小车位置
     def set_Location(self):
-        for j in range(len(self.mapinfor["mkList"]["id"])):
-            if self.mapinfor["mkList"]["id"][j] == self.localmark :
+        for j in range(len(self.mapinfor["mkList"])):
+            if self.mapinfor["mkList"][j]["id"] == self.localmark :
                n = j
-               if self.mu >= self.mapinfor["mkList"]["dist"][n] :
+               if self.mu >= self.mapinfor["mkList"][n]["dist"] :
                   self.location ='arr'                  
         else:
            self.location = 'nar'
 
-
+#   创建多个socket
     def create_socket(self, context=None):
         context = zmq.Context().instance()
         self.socket_cl1 = context.socket(zmq.SUB)
-#        self.socket_cl1.connect(self.test3)            # test
-        self.socket_cl1.connect(self.cc_endpoint)
+        self.socket_cl1.connect(self.test3)            # test
+#        self.socket_cl1.connect(self.cc_endpoint)
         self.socket_cl1.setsockopt(zmq.SUBSCRIBE, b'')
 
         context = zmq.Context().instance()
@@ -119,7 +123,7 @@ class PF_Location():
         self.socket_marl.connect(self.mark_endpoint)
         self.socket_marl.setsockopt(zmq.SUBSCRIBE, b'')
 
-
+#   运行部分，单进程
     def PFrun(self, N = 5000, sensor_std_err = 0.1):
         while True:
         #   cc to loc
@@ -133,12 +137,15 @@ class PF_Location():
                 self.iddata = self.socket_cl1.recv_json() 
                 self.localmark = self.iddata["tar_dest"][0]
             print('location recieved: ', self.localmark)
+
         #   loc to map and map to loc
             mapmark = {"id":self.localmark}
             self.socket_ml.send_json(mapmark)
             self.mapinfor = self.socket_ml.recv_json()
-            max = self.mapinfor["mkList"]["dist"][-1]
+            max = self.mapinfor["mkList"][-1]['dist']*100
+            print(max)
             particles = self.create_uniform_particles(max, N) ## max的值，需要具体接口知道实际地图的大小
+
             weights = np.zeros(N)
             landmarks =[]
             targetID =None
@@ -146,66 +153,84 @@ class PF_Location():
                 #   mark  to  loc
                 nowid = None
                 count = 0
+                self.marker_data = self.socket_marl.recv_json()
                 while len(self.marker_data["dists"]) == 0 and count < 10:
                    self.marker_data = self.socket_marl.recv_json()
                    count += 1
                    
                 print(self.marker_data["dists"])
                 print("localmark", self.localmark)
-                if (self.marker_data) and (self.localmark is not None) :
+#               限制条件，需要在有目标mark和小车在看的到mark的情况下进行，如果看不到，粒子无法更新权值 只能移动
+                if (self.marker_data) and (self.localmark is not None) : 
                     self.set_direction() 
                     targetID2 = targetID
                     targetID = self.marker_data["ids"][0][0]
+#               判断小车视野里是否有目标mark 如果有，直接锁定目标mark                
                     m = None
                     for i in range(len(self.marker_data["ids"])) :
                         if(self.marker_data["ids"][i][0]==self.localmark):
                            nowid = self.localmark
                            m = i
-                           for j in range(len(self.mapinfor["mkList"]["id"])):
-                               if self.mapinfor["mkList"]["id"][j] == self.localmark :                             
-                                   marklocal=self.mapinfor["mkList"]["dist"][j]
+                           
+                           for j in range(len(self.mapinfor["mkList"])):
+                               if self.mapinfor["mkList"][j]["id"] == self.localmark :                             
+                                   marklocal=self.mapinfor["mkList"][j]["dist"]
                                    landmarks.append(marklocal)
+#               小车视野里没有目标mark时，就用视野中其它mark作为判定位置的依据
                     if m is None:
                         if(targetID2 != targetID):
                             if(len(landmarks)):
                                 landmarks.pop()
-                            for j in range(len(self.mapinfor["mkList"]["id"])):
-                                if self.mapinfor["mkList"]["id"][j] == targetID :
+                            for j in range(len(self.mapinfor["mkList"])):
+                                if self.mapinfor["mkList"][j]["id"][j] == targetID :
                                     s = j
-                            marklocal=self.mapinfor["mkList"]["dist"][s]
+                            marklocal=self.mapinfor["mkList"][s]["dist"]
                             landmarks.append(marklocal)
                             nowid=targetID
+
                     print("Now TGmark is %s"%(nowid))
                     print(self.direction)
                 xs = []
                 #   设置时间：
                 self.time2 = self.time1
-                while len(self.iddata["time"]) == 0 :
+                self.timedata = self.socket_cl2.recv_json() 
+                while len(self.timedata['time']) == 0 :
                     self.timedata = self.socket_cl2.recv_json() 
-                    self.time1 = self.iddata["time"][0]
+                    self.time1 = self.timedata['time'][0]
                 time = self.time1 - self.time2
-                dire = self.direction
+
+#               状态更新模块
+                dire = self.direction 
                 if dire is not None:                
                     self.predict(particles, u=51, std = 0.2, dt = time,dir = dire) ##单位厘米 51厘米每秒
                 if dire is None:
                     self.predict(particles, u=51, std = 0.2, dt = time,dir = 1)
+
+#               权值更新模块
                 if(len(landmarks)):
                     if m is not None :
                        ds = self.marker_data["dists"][m][0]
                     else:
                        ds = self.marker_data["dists"][0][0]
-                    self.update(particles, weights, z=ds, R=sensor_std_err, landmarks=landmarks)##更新粒子权值
+                    dz = [ds]
+                    self.update(particles, weights, z=dz, R=sensor_std_err, landmarks=landmarks)##更新粒子权值
+
                 if self.neff(weights) < N / 2 :##判断是否需要重采样
                    self.simple_resample(particles, weights)
+
+#               得出预测小车的位置                   
                 self.mu, self.var = self.estimate(particles, weights)
                 xs.append(self.mu)
                 print("Get the current car position as follows : %s" %self.mu)
                 self.set_Location()  
+
 #               传输部分：
                 location_date={"direction":self.direction,"location":self.location}
                 self.socket_lc.send(location_date)
                 self.direction = None
                 # time.sleep(3)
+
+
     def run(self) :
         self.create_socket()
         self.PFrun()
